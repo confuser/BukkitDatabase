b0c2f8d438c32d8bdd954e163435fcebb5ebb66e
confuser <jamesmortemore@gmail.com>
2014-01-12 14:24:25 +0000

    Initial Commit

---
+++ src/me/confuserr/database/SQLQueue.java
@@ -0,0 +1,19 @@
+package me.confuserr.database;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class SQLQueue {
+	private ConcurrentLinkedQueue<StoredStatement> storedStatements = new ConcurrentLinkedQueue<StoredStatement>();
+
+	public void add(StoredStatement statement) {
+		storedStatements.add(statement);
+	}
+
+	public StoredStatement next() {
+		return storedStatements.remove();
+	}
+	
+	public boolean hasNext() {
+		return storedStatements.size() > 0;
+	}
+}
